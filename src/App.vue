c9783dc6370f3f576fab96e9b778bfe6ac69e631
“wangyihan” <1229128173@qq.com>
2022-10-15 22:24:25 +0800

    初始化vue2-music

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
+#app {
+  color: $color-theme;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App";
+
+import "common/stylus/index.styl";
+// eslint-disable-next-line no-multiple-empty-lines
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  render: h => h(App)
+});
